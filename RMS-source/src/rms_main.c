e23fa411fc0673b5f169b305d4fa9c2746e3fc72
afaraz623 <afaraz623@gmail.com>
2022-05-23 22:14:42 +0500

    cleaned up rms_main.c

--- RMS-source/src/rms_main.c
+++ RMS-source/src/rms_main.c
@@ -1,6 +1,7 @@
 #include <../../../f429-RMSv3-autogen/Core/Inc/main.h>
 #include <../../../f429-RMSv3-autogen/Core/Inc/gpio.h>
 
+// Prototypes
 extern void SystemClock_Config(void);
 
 int main(void)
@@ -8,12 +9,10 @@ int main(void)
     HAL_Init();
     SystemClock_Config();
     MX_GPIO_Init();
+
     while (1)
     {
-        HAL_GPIO_TogglePin(led_1_GPIO_Port, led_1_Pin);
-        HAL_Delay(2000);
-        HAL_GPIO_TogglePin(led_2_GPIO_Port, led_2_Pin);
-        HAL_Delay(2000);
+
     }
     
 }
