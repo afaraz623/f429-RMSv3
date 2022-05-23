2ab25d9552a3cec8c213ae7a8ba95898a4181ec8
afaraz623 <afaraz623@gmail.com>
2022-05-24 00:23:09 +0500

    Added microros

--- RMS-source/src/rms_main.c
+++ RMS-source/src/rms_main.c
@@ -1,18 +1,30 @@
-#include <../../../f429-RMSv3-autogen/Core/Inc/main.h>
-#include <../../../f429-RMSv3-autogen/Core/Inc/gpio.h>
+#include "main.h"
+#include "gpio.h"
+#include "cmsis_os.h"
+#include "dma.h"
+#include "usart.h"
 
 // Prototypes
 extern void SystemClock_Config(void);
+extern void MX_FREERTOS_Init(void);
 
 int main(void)
 {
+    // Reset of all peripherals, Initializes the Flash interface and the Systick
     HAL_Init();
+
+    // Configure the system clock
     SystemClock_Config();
+
+    // Initialize all configured peripherals
     MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_USART3_UART_Init();
 
-    while (1)
-    {
+    // Call init function for freertos objects (in freertos.c)
+    osKernelInitialize();  
+    MX_FREERTOS_Init();
 
-    }
-    
+    //Start scheduler
+    osKernelStart();    
 }
